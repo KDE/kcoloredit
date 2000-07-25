307146be55c6cbf08662389eb6cfd3ba3bf29e8c
Waldo Bastian <bastian@kde.org>
2000-07-25 00:27:18 +0000

    WABA: Added KColorChooser, a stand alone KColorDialog
    svn path=/trunk/kdegraphics/kcoloredit/; revision=58611

---
+++ kcolorchooser.cpp
@@ -0,0 +1,57 @@
+/*
+This file is part of KDE 
+
+  Copyright (C) 1998-2000 Waldo Bastian (bastian@kde.org)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <kapp.h>
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
+#include <klocale.h>
+
+#include <kcolordlg.h>
+
+static const char *description =
+	I18N_NOOP("KDE Color Chooser");
+
+static const char *version = "v1.0";
+
+	
+int main(int argc, char *argv[])
+{
+
+  KAboutData aboutData( "kcolorchooser", I18N_NOOP("KColorChooser"),
+		version, description, KAboutData::License_BSD,
+		"(c) 2000, Waldo Bastian");
+  aboutData.addAuthor("Waldo Bastian",0, "bastian@kde.org");
+  KCmdLineArgs::init( argc, argv, &aboutData );
+
+  KApplication app;
+ 
+  KColorDialog dlg;
+
+  dlg.setColor(Qt::blue); // Just a color
+
+  app.connect(&dlg, SIGNAL(finished()), SLOT(quit()));
+
+  dlg.show();
+  app.exec();
+
+}  
