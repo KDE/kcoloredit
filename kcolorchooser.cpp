be74827f879440dab001bf7d2911b9c63001098e
Richard Moore <rich@kde.org>
2003-09-28 12:22:08 +0000

    Added a print option so this thing is actually useful
    svn path=/trunk/kdegraphics/kcoloredit/; revision=254563

--- kcolorchooser.cpp
+++ kcolorchooser.cpp
@@ -27,12 +27,18 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <klocale.h>
 
 #include <kcolordialog.h>
+#include <stdio.h>
 
 static const char *description =
 	I18N_NOOP("KDE Color Chooser");
 
 static const char *version = "v1.0";
 
+static KCmdLineOptions options[] =
+{
+   { "print", I18N_NOOP("Print the selected color to stdout"), 0 },
+   KCmdLineLastOption
+};
 	
 int main(int argc, char *argv[])
 {
@@ -42,6 +48,7 @@ int main(int argc, char *argv[])
 		"(c) 2000, Waldo Bastian");
   aboutData.addAuthor("Waldo Bastian",0, "bastian@kde.org");
   KCmdLineArgs::init( argc, argv, &aboutData );
+  KCmdLineArgs::addCmdLineOptions( options );
 
   KApplication app;
   
@@ -54,4 +61,9 @@ int main(int argc, char *argv[])
   dlg.show();
   app.exec();
 
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  QColor c = dlg.color();
+  if ( args->isSet("print") && c.isValid() ) {
+      printf( "#%x%x%x", c.red(), c.green(), c.blue() );
+  }
 }  
