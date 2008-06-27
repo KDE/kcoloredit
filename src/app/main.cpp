b8f7d5cfae64b2c56ba8b4321d6f5c80a4b792c9
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2008-06-27 01:17:57 +0000

    CCMAIL: montel@kde.org
    change reverted, please note the the paremeter name is used in the main function.
    
    svn path=/trunk/extragear/graphics/kcoloredit/; revision=824982

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -27,7 +27,7 @@
 
 inline KAboutData kcoloreditAboutData( const char* name, const char* iname )
 {
-    KAboutData about("kcoloredit", 0, ki18n(iname), "1.70.00 (KColorEdit 1.7)",
+    KAboutData about(name, 0, ki18n(iname), "1.70.00 (KColorEdit 1.7)",
         ki18n(I18N_NOOP("KColorEdit, great tool for editing color palettes.")),
         KAboutData::License_GPL, ki18n("(C) 2008, Percy Camilo Triveño Aucahuasi\n" "(C) 2000, Artur Rataj"));
     about.addAuthor(ki18n("Percy Camilo Triveño Aucahuasi"), ki18n("Current maintainer - Upgraded KColorEdit to KDE 4"), "orgyforever@gmail.com");
